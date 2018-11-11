edda676b12be4f38c798e4ed927d56dfcd094556
Kyle Ohanian <kyle.ohanian@willowtreeapps.com>
2018-11-11 15:59:14 -0500

    Initial Commit

---
+++ Hack110AwesomeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hack110AwesomeApp
+//
+//  Created by Kyle Ohanian on 11/11/18.
+//  Copyright © 2018 Hack110Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
